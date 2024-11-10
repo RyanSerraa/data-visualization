c380c5c0188648fbf0dcc074c36e911e656f8700
Daniel Rodrigues Ribeiro <daniel.rodrigues@g.globo>
2024-11-10 14:37:32 -0300

    DEV-002: Gráfico de Dispersão

---
+++ 14 - d3 (eixos)/index.js
@@ -0,0 +1,156 @@
+class GraficoDispersao {
+  constructor(config) {
+    this.config = config;
+
+    this.svg = null;
+    this.margens = null;
+
+    this.intervaloX = null;
+    this.intervaloY = null;
+
+    this.circulos = []
+
+    this.createSvg();
+    this.createMargins();
+  }
+
+  createSvg() {
+    this.svg = d3.select(this.config.div)
+      .append("svg")
+      // .attr('x', 10)
+      // .attr('y', 10)
+      // .attr('width', this.config.width + this.config.left + this.config.right)
+      // .attr('height', this.config.height + this.config.top + this.config.bottom)
+      // .attr("max-width", this.config.width)
+      .attr("viewBox", [0, 0, this.config.width + this.config.left + this.config.right , this.config.height + this.config.top + this.config.bottom])
+      .style("max-width", `${this.config.width}px`)
+      // .style("border", "1px solid black");
+      const legenda = document.querySelector(".legenda");
+      legenda.style.marginTop = `${this.config.height - this.config.bottom - this.config.top- this.config.left - this.config.right}px`;
+  }
+
+  createMargins() {
+    this.margens = this.svg
+      .append('g')
+  }
+
+  async loadJSON(file) {
+    this.circulos = await d3.json(file);
+    this.circulos = this.circulos.map(d => {
+      return {
+        cx: +d.Profit,
+        cy: +d.Sales,
+        col: d.Discount,
+        cat: d.Category,
+        r: 4
+      }
+    });
+
+    this.circulos = this.circulos.slice(0, 1000);
+  }
+
+  createScales() {
+    let xExtent = d3.extent(this.circulos, d => {
+      return d.cx;
+    });
+    let yExtent = d3.extent(this.circulos, d => {
+      return d.cy;
+    });
+    let colExtent = d3.extent(this.circulos, d => {
+      return d.col;
+    });
+
+    const cats = this.circulos.map(d => {
+      return d.cat;
+    });
+    let catExtent = d3.union(cats);
+
+    this.intervaloX = d3.scaleLinear().domain(xExtent).nice().range([this.config.left, this.config.width - this.config.right]);
+    this.intervaloY = d3.scaleLinear().domain(yExtent).nice().range([this.config.height-this.config.bottom, this.config.top]);
+
+    this.colScale = d3.scaleSequential(d3.interpolateOrRd).domain(colExtent);
+    this.catScale = d3.scaleOrdinal().domain(catExtent).range(d3.schemeTableau10);
+    // const categorias = this.catScale.domain(); // Obtém as categorias definidas no domínio
+    // categorias.forEach(categoria => {
+    //     const cor = this.catScale(categoria); // Obtém a cor associada a cada categoria
+    //     console.log(`Categoria: ${categoria}, Cor: ${cor}`);
+    // });
+  }
+
+  createAxis() {
+    let xAxis = d3.axisBottom(this.intervaloX.copy().interpolate(d3.interpolateRound))
+
+    let yAxis = d3.axisLeft(this.intervaloY.copy().interpolate(d3.interpolateRound))
+
+    // let xExtent = d3.extent(this.circulos, d => {
+    //   return d.cx;
+    // });
+    // let yExtent = d3.extent(this.circulos, d => {
+    //   return d.cy;
+    // });
+
+    this.margens
+      .append("g")
+      .attr("transform", `translate(10,${this.config.height-8})`)
+      .call(xAxis)
+      // .call(g => g.select(".domain").remove())
+      .call(g => g.selectAll(".tick line").clone()
+          .attr("stroke-opacity", 0.5)
+          .attr("y1", -this.config.height + this.config.top + this.config.bottom))
+      .call(g => g.append("text")
+          .attr("x", this.config.width - this.config.right+15)
+          .attr("y", 1)
+          .attr("fill", "currentColor")
+          .attr("font-weight", "bold")
+          .text("Lucro"));
+      
+
+    this.margens
+      .append("g")
+      .attr("transform", `translate(${this.config.left+10},${this.config.top})`)
+      .call(yAxis)
+      // .call(g => g.select(".domain").remove())
+      .call(g => g.selectAll(".tick line").clone()
+          .attr("stroke-opacity", 0.5)
+          .attr("x1", this.config.width - this.config.left - this.config.right))
+      .call(g => g.select(".tick:last-of-type text").clone()
+          .attr("x", 0)
+          .attr('y', -5)
+          .attr("text-anchor", "start")
+          .attr("font-weight", "bold")
+          .text("Valor da venda"));
+
+    // this.margens.append("line")
+    //   .attr("stroke", "currentColor")
+    //   .attr("x1", this.intervaloX(xExtent[0]))
+    //   .attr("x2", this.intervaloX(xExtent[1]))
+    //   .attr("y1", this.intervaloY(yExtent[0]))
+    //   .attr("y2", this.intervaloY(yExtent[1]));
+  }
+
+  renderCircles() {
+    this.margens.selectAll('circle')
+      .data(this.circulos)
+      .join('circle')
+      .attr('cx', d => this.intervaloX(d.cx))
+      .attr('cy', d => this.intervaloY(d.cy))
+      .attr('r' , d => d.r)
+      .attr('fill', d => this.colScale(d.col))
+      .attr('stroke', 'black')
+      // .attr('fill', d => this.catScale(d.cat));
+  }
+}
+
+
+async function main() {
+  let c = {div: '#main', width: 640, height: 640, top: 20, left: 40, bottom: 30, right: 40};
+  
+  let a = new GraficoDispersao(c);
+  await a.loadJSON('./superstore.json');
+  
+  a.createScales();
+  a.createAxis();
+  a.renderCircles();
+}
+
+main();
