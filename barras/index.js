75f04e97f5cd2a90c739ad0cf1a8552b1375b730
Ryan Serra <ryan16serrasilva@gmail.com>
2024-11-15 10:59:25 -0300

    DEV-004 Colocando a descrição das colunas

--- barras/index.js
+++ barras/index.js
@@ -1,5 +1,5 @@
 class BarrasJs {
-    constructor({ width = 800, height = 600, margin = { top: 20, right: 30, bottom: 250, left: 300 } } = {}) {
+    constructor({ width = 800, height = 600, margin = { top: 20, right: 150, bottom: 250, left: 300 } } = {}) {
         this.w = width;
         this.h = height;
         this.margin = margin;
@@ -40,20 +40,20 @@ class BarrasJs {
     drawChart() {
         const width = this.w - this.margin.left - this.margin.right;
         const height = this.h - this.margin.top - this.margin.bottom;
-
+    
         const xScale = d3.scaleBand()
             .domain(this.data.map(d => d.product))
             .range([0, width])
             .padding(0.1);
-
+    
         const yScale = d3.scaleLinear()
             .domain([0, d3.max(this.data, d => d.quantity)])
             .nice()
             .range([height, 0]);
-
+    
         const g = this.svg.append("g")
             .attr("transform", `translate(${this.margin.left},${this.margin.top})`);
-
+    
         g.selectAll("rect")
             .data(this.data)
             .join("rect")
@@ -62,16 +62,35 @@ class BarrasJs {
             .attr("width", xScale.bandwidth())
             .attr("height", d => height - yScale(d.quantity))
             .style("fill", "RoyalBlue");
-
+    
+        // Eixo X
         g.append("g")
             .attr("transform", `translate(0,${height})`)
             .call(d3.axisBottom(xScale))
             .selectAll("text")
             .attr("transform", "rotate(-45)")
             .style("text-anchor", "end");
-
+    
+        // Eixo Y
         g.append("g")
             .call(d3.axisLeft(yScale));
+    
+        // Rótulo do eixo X
+        this.svg.append("text")
+            .attr("x", this.margin.left + width +25)
+            .attr("y", this.h - this.margin.bottom )
+            .attr("text-anchor", "middle")
+            .style("font-size", "12px")
+            .text("Produtos");
+    
+        // Rótulo do eixo Y
+        this.svg.append("text")
+            .attr("x", -(this.margin.top + height / 2))
+            .attr("y", this.margin.left - 60)
+            .attr("transform", "rotate(-90)")
+            .attr("text-anchor", "middle")
+            .style("font-size", "12px")
+            .text("Quantidade");
     }
 
     async run(selector, file) {
